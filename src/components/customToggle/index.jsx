7788b9dd97446b8a6ef7a1a5035b0164eae5d052
User <user@Users-MacBook-Air.local>
2021-06-08 17:16:05 +0300

    redo btn styles

--- src/components/customToggle/index.jsx
+++ src/components/customToggle/index.jsx
@@ -1,20 +1,11 @@
 import { useAccordionToggle } from "react-bootstrap"
+import "./styles.css"
 
 function CustomToggle({ children, eventKey }) {
 	const decoratedOnClick = useAccordionToggle(eventKey, () => {})
 
 	return (
-		<button
-			type='button'
-			style={{
-				backgroundColor: "#0d6efd",
-				padding: "5px 40px",
-				outline: "none",
-				border: "1px solid #0d6efd",
-
-				color: "white",
-			}}
-			onClick={decoratedOnClick}>
+		<button type='button' className='button' onClick={decoratedOnClick}>
 			{children}
 		</button>
 	)
