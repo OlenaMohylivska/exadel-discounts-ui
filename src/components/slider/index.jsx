66e501168138214334fbb4919ab9602eb32afe31
Olena Mohylivska <mogilevskaya1994@gmail.com>
2021-06-08 16:03:42 +0300

    done

--- src/components/slider/index.jsx
+++ src/components/slider/index.jsx
@@ -1,8 +1,30 @@
-import React from "react"
-import "./styles.css"
+import React from 'react';
+import { Carousel } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import Image1 from '../../assets/1.jpg';
+import Image2 from '../../assets/2.jpg';
+import Image3 from '../../assets/3.jpg';
+import './styles.css';
 
 const Slider = () => {
-	return <div className='slider'>top Promotions</div>
+  const sliderImages = [Image1, Image2, Image3];
+  return (
+    <Carousel prevLabel={null} nextLabel={null} pause={false} interval={4000} className="carousel mt-2 mb-2 ">
+      {sliderImages.map((image, index) => {
+        return (
+          <Carousel.Item key={index + 1}>
+            <Link to="/Login">
+              <img
+                className="d-inline-block w-100 img"
+                src={image}
+                alt={`Slide ${index + 1}`}
+              />
+            </Link>
+          </Carousel.Item>
+        )
+      })}
+    </Carousel>
+  )
 }
 
 export default Slider
