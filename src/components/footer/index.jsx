6398e22de8085ad0f724ad3007c052862a8f85a1
Oleh <olehfromukraine@gmail.com>
2021-06-09 09:55:03 +0300

    created footer structure

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -2,15 +2,58 @@ import React from "react"
 import "./styles.css"
 
 function Footer() {
-	return (
-		<div className='footer-container'>
-			<div style={{ padding: "30px" }}>
-				if you have any problems with our website
-				<br />
-				Our Contacts: 88000770007
-			</div>
-		</div>
-	)
+return (
+<div class="row">
+   <div class="column">
+      <p>Get to Know Us</p>
+      <p><a href="#">Careers</a></p>
+      <p><a href="#">Blog</a></p>
+      <p><a href="#">About us</a></p>
+   </div>
+   <div class="column">
+      <p>Make Money with Us</p>
+      <p><a href="#">Self-Publish with Us</a></p>
+   </div>
+   <div class="column">
+      <p>Products</p>
+      <p><a href="#">Business Advice</a></p>
+   </div>
+   <div class="column">
+      <p>Let Us Help You</p>
+      <p><a href="#">Your Account</a></p>
+      <p><a href="#">Help</a></p>
+   </div>
+   <div class="lower">
+      <div class="column">
+         <img src="images/logo.png" 
+            width="100" height="100" alt="logo"/>
+      </div>
+      <div class="column">
+         <select>
+            <option>English</option>
+            <option>Ukrainian</option>
+            <option>Polish</option>
+            <option>Russian</option>
+         </select>
+      </div>
+      <div class="column">
+         <select>
+            <option>USD</option>
+            <option>UAH</option>
+            <option>PLN</option>
+            <option>RUB</option>
+         </select>
+         <div class="column">
+            <select>
+               <option>United State</option>
+               <option>Ukraine</option>
+               <option>Polish</option>
+               <option>Russia</option>
+            </select>
+         </div>
+      </div>
+   </div>
+</div>
+)
 }
-
 export default Footer
