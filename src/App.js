d3c7609755a944611ca6a24323258e11acabe884
Olena Mohylivska <mogilevskaya1994@gmail.com>
2021-07-01 09:44:55 +0300

    N112/corrected according to comment

--- src/App.js
+++ src/App.js
@@ -2,13 +2,10 @@ import React from 'react'
 import { BrowserRouter, Switch } from 'react-router-dom'
 import { Context, ContextImages } from './store/context'
 import AppRouter from './router'
-
 import Header from './components/header'
 import Footer from './components/footer'
-
+import { productImages } from 'store/constants'
 import './App.scss'
-
-
 
 function App() {
 
@@ -16,18 +13,16 @@ function App() {
 
   return (
     <Context.Provider value={{ contextCheck: contextCheck }}>
-      <ContextImages.Consumer>
-        {() => (
-          <BrowserRouter>
-            <Header />
-            <Switch>
-              <AppRouter />
-            </Switch>
-            <div>Version: 25.06.2021</div>
-            <Footer />
-          </BrowserRouter>
-        )}
-      </ContextImages.Consumer>
+      <ContextImages.Provider value={productImages}>
+        <BrowserRouter>
+          <Header />
+          <Switch>
+            <AppRouter />
+          </Switch>
+          <div>Version: 25.06.2021</div>
+          <Footer />
+        </BrowserRouter>
+      </ContextImages.Provider>
     </Context.Provider>
   )
 }
