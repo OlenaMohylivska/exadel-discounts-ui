9fae9296000771100301e7c05a809bf192ab432d
Slava Zhuravel <slavonzhuravel@gmail.com>
2021-07-02 13:02:20 +0300

    showing other users' reviews on discount page

--- src/views/discount-page/index.jsx
+++ src/views/discount-page/index.jsx
@@ -4,7 +4,7 @@ import { Button, Form } from "react-bootstrap"
 import StarRatings from "react-star-ratings"
 import * as axios from "axios"
 import FetchError from "../../components/fetch-error"
-import "./styles.css"
+import "./styles.scss"
 
 const baseUrl = process.env.REACT_APP_BASE_BACKEND_URL
 
@@ -17,7 +17,9 @@ const DiscountPage = () => {
   const { image } = location.state ? location.state : ""
   const { id } = useParams()
   const [reviewText, setReviewText] = useState("")
+  const [rating, setRating] = useState(0)
   const [review, setReview] = useState(null)
+  const [allReviews, setAllReviews] = useState([])
 
   const fetchData = async () => {
     setLoading(true)
@@ -39,10 +41,28 @@ const DiscountPage = () => {
   }, [])
 
   useEffect(() => {
+    setLoading(true)
+    try {
+      axios
+        .get(`${baseUrl}/api/discounts/${id}/reviews`)
+        .then((response) => {
+          setAllReviews(response.data)
+        })
+      setLoading(false)
+    } catch (e) {
+
+      setErrorMessage(e.message)
+
+    } finally {
+      setLoading(false)
+    }
+  }, [])
+
+  useEffect(() => {
     setReview({
-      rate: 5,
+      rate: rating,
       comment: reviewText,
-      date: 1274389200000,
+      date: new Date(),
       discount: discount,
       employee: {
         id: 2,
@@ -51,24 +71,55 @@ const DiscountPage = () => {
         role: "USER"
       }
     })
-  }, [reviewText])
+  }, [reviewText, rating])
 
   const handleReviewText = e => {
     setReviewText(e.target.value)
+  }
+  const handleRating = value => {
+    setRating(value)
   }
 
   const addReview = () => {
     axios.post(baseUrl + "/api/reviews", review)
     setReviewText("")
+    setRating(0)
   }
+
   return (
     <>
       {loading ? <div>Loading</div> : ""}
       {discount ? (
-        <div className="container discount-container d-flex align-items-center flex-wrap">
+        <div className="container discount-container flex-wrap">
           <div className="col-lg-6 col-md-12">
             <div className="img-container">
               <img src={image} className="discount-image" alt="discount-img" />
+            </div>
+
+            <div>
+              <div className="rates">
+                <StarRatings starDimension="27px" starSpacing="5px" rating={rating} changeRating={handleRating} starRatedColor="#FFD700" />
+              </div>
+              <div className="action">
+                <div>
+                  <Form.Group>
+                    <Form.Control className="mb-3" as="textarea" rows={4} cols={50} value={reviewText} onChange={handleReviewText}>
+
+                    </Form.Control>
+
+                  </Form.Group>
+                  <Button className="d-flex align-self-start" variant="dark" onClick={() => { addReview() }}>Leave feedback</Button>
+                </div>
+                <Button className="w-25 d-flex align-self-end justify-content-center" onClick={() => setShow(!show)} variant="primary">
+                  Order
+                </Button>
+
+              </div>
+              <div className="d-flex justify-content-end">
+                <p className={`${!show ? "hide" : "display"}`}>
+                  {discount.promoCode}
+                </p>
+              </div>
             </div>
           </div>
           <div className="col-lg-6 col-md-12">
@@ -95,29 +146,52 @@ const DiscountPage = () => {
             </h4>
             <h4>Description:</h4>
             <p>{discount.description}</p>
-            <div className="rates">
-              <StarRatings starDimension="27px" starSpacing="5px" />
-            </div>
-            <div className="action">
-              <div>
-                <Form.Group>
-                  <Form.Control className="mb-3" as="textarea" rows={4} cols={50} value={reviewText} onChange={handleReviewText}>
 
-                  </Form.Control>
+            <div >
+              {allReviews.length ?
+                <div>
+                  <h4>Reviews:</h4>
+                  {allReviews.map(review => {
+                    return (
 
-                </Form.Group>
-                <Button className="d-flex align-self-start" variant="dark" onClick={() => { addReview() }}>Leave feedback</Button>
-              </div>
-              <Button className="w-25 d-flex align-self-end justify-content-center" onClick={() => setShow(!show)} variant="primary">
-                Order
-              </Button>
+                      <div key={review.id} className="review">
+                        <div className="d-flex justify-content-between">
+                          <div className="m-2">
+                            <img
+                              className="user-image"
+                              src="https://i.pinimg.com/originals/17/56/8f/17568fcd478e0699067ca7b9a34c702f.png"
+                              alt="user-image"
+                            />
+                            <p className="d-inline mx-3">{review.employee.login}</p>
+                          </div>
 
+                          <p>{review.employee.location || ""}</p>
+                          <p className="my-2"> {new Date(review.date)
+                            .toISOString()
+                            .split(":")
+                            .splice(0, 1)
+                            .join("")
+                            .split("")
+                            .splice(0, 10)
+                            .join("")
+                            .split("-")
+                            .reverse()
+                            .join("-")}</p>
+                        </div>
+                        <div className="d-flex justify-content-between align-items-end">
+                          <p className="comment">{review.comment}</p>
+                          <div className="rates align-self-end">
+                            <StarRatings starDimension="24px" starSpacing="4px" rating={review.rate} starRatedColor="#FFD700" />
+                          </div>
+
+                        </div>
+                      </div>
+                    )
+                  })}
+
+                </div> : ""}
             </div>
-            <div className="d-flex justify-content-end">
-              <p className={`${!show ? "hide" : "display"}`}>
-                {discount.promoCode}
-              </p>
-            </div>
+
           </div>
         </div>
       ) : (
