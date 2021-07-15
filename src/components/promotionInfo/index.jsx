a3d27bf411bce777371d27067a5efa24de90a889
Natalie <nataly.senyk@gmail.com>
2021-07-15 21:52:11 +0300

    Fixed All promotions tab and did some adjusmtmetns for images there, added spinner

--- src/components/promotionInfo/index.jsx
+++ src/components/promotionInfo/index.jsx
@@ -1,16 +1,36 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { Container, Card, Button } from "react-bootstrap"
 import StarRatings from "react-star-ratings"
 import PropTypes from "prop-types"
 import { Link, useHistory, useRouteMatch } from "react-router-dom"
 import "./styles.scss"
 import moment from "moment"
+import axiosInstance from "components/api"
+
+const baseUrl = process.env.REACT_APP_BASE_BACKEND_URL
 
 const PromotionInfo = ({ elem }) => {
+  const [imgName, setImgName] = useState(null)
+  const [imgUrl, setImgUrl] = useState(null)
+  let blob = new Blob([imgName], { type: "image/jpeg" })
+  const url = blob && URL.createObjectURL(blob)
+
   const formattedData = moment(elem.periodEnd).format("MMM Do YYYY")
 
   const history = useHistory()
   const { path } = useRouteMatch()
+
+  useEffect(async () => {
+    await axiosInstance
+      .get(`${baseUrl}/api/images`)
+      .then((response) => setImgName(response.data.name))
+  }, [elem])
+
+  useEffect(async () => {
+    await axiosInstance
+      .get(`${baseUrl}/api/images/${imgName}`)
+      .then((response) => setImgUrl(response.data))
+  }, [elem])
 
   const updateItemHandler = () => {
     history.push(`${path}/edit-item/${elem.id}`)
@@ -18,7 +38,7 @@ const PromotionInfo = ({ elem }) => {
 
   return (
     <Container className="discount-wrapper">
-      <Card className=" shadow product-card">
+      <Card className=" shadow product-card border-0">
         <Link
           key={elem.id}
           to={{
@@ -32,7 +52,15 @@ const PromotionInfo = ({ elem }) => {
             expires in {formattedData}
           </Card.Subtitle>
           <Card.Title className="mb-3 card-title">{elem.name}</Card.Title>
-          <Card.Img variant="top" className="product-image" src={elem.img} />
+          {imgUrl ? (
+            <Card.Img variant="top" className="product-image" src={url} />
+          ) : (
+            <Card.Img
+              variant="top"
+              className="product-image"
+              src="https://img.icons8.com/plasticine/2x/no-image.png"
+            />
+          )}
         </Link>
         <Card.Body className="p-0 d-flex flex-column justify-content-between">
           <div>
@@ -63,6 +91,7 @@ export default PromotionInfo
 PromotionInfo.propTypes = {
   elem: PropTypes.shape({
     id: PropTypes.number,
+    imageId: PropTypes.number,
     periodEnd: PropTypes.number,
     name: PropTypes.string,
     description: PropTypes.string,
