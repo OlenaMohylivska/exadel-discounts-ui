40453be0ec44ef17488227cf1f1795f0cb4c0595
bogdan.taradada <reyyes98@gmail.com>
2021-07-20 11:19:41 +0300

    new commit

--- src/App.js
+++ src/App.js
@@ -6,16 +6,13 @@ import AppRouter from "./router"
 import Header from "./components/header"
 import Footer from "./components/footer"
 import { productImages } from "store/constants"
-
 import "./App.scss"
 import Breadcrumbs from "components/breadcrumbs"
 
 function App() {
-  const token = localStorage.getItem("jwt")
-  const [isAuthorized, setIsAuthorized] = useState(!!token)
-  useEffect(() => {
+  const [isAuthorized, setIsAuthorized] = useState(false)
   const bindToken = () => {
-    const token = !!localStorage.getItem("jwt")
+    const token = localStorage.getItem("jwt") && localStorage.getItem("jwt")
     axiosInstance.interceptors.request.use((config) => {
       token ? (config.headers.Authorization = token) : config
       return config
