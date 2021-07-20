427d4624245088bba0d0b92bf08c0333f4254c69
Slava Zhuravel <slavonzhuravel@gmail.com>
2021-07-20 22:10:05 +0300

    done

--- src/components/companyInfo/index.jsx
+++ src/components/companyInfo/index.jsx
@@ -1,62 +1,56 @@
-import React, { useState, useEffect, useContext } from "react"
+import React, { useEffect, useContext } from "react"
 import { Container, Card, Button } from "react-bootstrap"
 import PropTypes from "prop-types"
 import { Link, useHistory, useRouteMatch } from "react-router-dom"
 import "./styles.scss"
-import axiosInstance from "components/api"
 import { Context } from "store/context"
 
-const CompanyInfo = ({ name, id }) => {
-  const [logo, setLogo] = useState("")
-  const baseUrl = process.env.REACT_APP_BASE_BACKEND_URL
+const baseUrl = process.env.REACT_APP_BASE_BACKEND_URL
+const discountDefaultImg = "https://zo.ua/uploads/no-logo.png"
+
+const CompanyInfo = ({ elem }) => {
+
   const history = useHistory()
   const { path } = useRouteMatch()
+
   const { bindToken } = useContext(Context)
   useEffect(() => {
     bindToken()
   }, [])
+
   const updateItemHandler = () => {
-    history.push(`${path}/edit-company/${id}`)
+    history.push(`${path}/edit-company/${elem.id}`)
   }
-
-  const fetchData = async (url, setFunc) => {
-    await axiosInstance
-      .get(baseUrl + url)
-      .then((response) => setFunc(response.data))
-  }
-  const token = localStorage.getItem("jwt") && localStorage.getItem("jwt")
-  useEffect(() => {
-    axiosInstance.interceptors.request.use((config) => {
-      token ? (config.headers.Authorization = token) : config
-      return config
-    })
-  }, [])
-
-  useEffect(() => {
-    fetchData(`/api/images/${id}`, setLogo)
-  }, [])
 
   return (
     <Container className="company-wrapper">
       <Card className="shadow product-card d-flex flex-column align-items-center card-size">
         <Link
-          key={id}
+          key={elem.id}
           to={{
-            pathname: `/edit-company/${id}`,
+            pathname: `/edit-company/${elem.id}`,
           }}
         >
           <Card.Title className="mb-3 card-title d-flex justify-content-center">
-            {name}
+            {elem.name}
           </Card.Title>
           <Card.Img
             variant="top"
-            className="product-image mb-5 logo-style"
-            src={logo ? logo : "https://zo.ua/uploads/no-logo.png"}
+            className="product-image mb-2 logo-style"
+            src={
+              elem.nameImage
+                ? `${baseUrl}/api/images/${elem.nameImage}`
+                : discountDefaultImg
+            }
           />
         </Link>
-        <Card.Body className="p-0 d-flex flex-column justify-content-between">
+        <Card.Body className="p-0 d-flex flex-column justify-content-between w-100 ">
           <div className="product-footer">
-            <Button variant="dark" onClick={updateItemHandler}>
+            <Button
+              className="company-update-btn"
+              variant="success"
+              onClick={updateItemHandler}
+            >
               Update
             </Button>
           </div>
@@ -69,6 +63,9 @@ const CompanyInfo = ({ name, id }) => {
 export default CompanyInfo
 
 CompanyInfo.propTypes = {
-  name: PropTypes.string,
-  id: PropTypes.number,
+  elem: PropTypes.shape({
+    name: PropTypes.string,
+    id: PropTypes.number,
+    nameImage: PropTypes.string
+  })
 }
