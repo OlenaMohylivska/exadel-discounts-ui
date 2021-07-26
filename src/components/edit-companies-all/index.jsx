23b719c178e9a0a0fecf53fe92537b4983ae502e
Olena Mohylivska <mogilevskaya1994@gmail.com>
2021-07-26 16:55:58 +0300

    N226/Add search to all companies

--- src/components/edit-companies-all/index.jsx
+++ src/components/edit-companies-all/index.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useContext } from "react"
 import axiosInstance from "components/api"
-
-import { Button, Spinner } from "react-bootstrap"
+import Loupe from "components/icons/Loupe"
+import { Button, Spinner, Form } from "react-bootstrap"
 import CompanyInfo from "components/companyInfo"
 import "./styles.scss"
 import { Redirect } from "react-router-dom"
@@ -15,23 +15,23 @@ const EditCompaniesAll = () => {
   const [newCompany, setNewCompany] = useState(false)
   const [companiesFetchError, setCompaniesFetchError] = useState(null)
   const { bindToken } = useContext(Context)
-  // const [search, setSearch] = useState({
-  //   pageNum: 0,
-  //   itemsPerPage: 10
-  // })
-  // const [isSearching, setIsSearching] = useState(false)
+  const [search, setSearch] = useState({
+    pageNum: 0,
+    itemsPerPage: 10
+  })
+  const [isSearching, setIsSearching] = useState(false)
 
-  // useEffect(() => {
-  //   if (isSearching) {
-  //     axiosInstance
-  //       .post("/api/company/search", search)
-  //       .then((res) => setCompanies(res.data.content))
-  //     setLoading(false)
-  //     setIsSearching(false)
-  //   } else {
-  //     return
-  //   }
-  // }, [isSearching])
+  useEffect(() => {
+    if (isSearching) {
+      axiosInstance
+        .post("/api/company/search", search)
+        .then((res) => setCompanies(res.data.content))
+      setLoading(false)
+      setIsSearching(false)
+    } else {
+      return
+    }
+  }, [isSearching])
 
 
 
@@ -59,19 +59,36 @@ const EditCompaniesAll = () => {
     setNewCompany(!newCompany)
   }
 
-  /* const handleSearchText = (event) => {
-    setSearch({...search, searchText: event.target.value})
+  const handleSearchText = (event) => {
+    setSearch({ ...search, searchText: event.target.value })
     setTimeout(funcDebouncer, 2000)
-  } */
+  }
 
-  // const funcDebouncer = () => {
-  //   setIsSearching(true)
-  //   setLoading(true)
-  // }
+  const funcDebouncer = () => {
+    setIsSearching(true)
+    setLoading(true)
+  }
 
   return (
     <div className="container">
       <div className="col-lg-12 col-md-12 my-4 search-container-wrapper justify-content-between">
+        <div className="col-lg-10 col-md-9">
+          <div className="position-relative w-100">
+            <label className="w-100">
+              <div className="search-icon">
+                <Loupe />
+              </div>
+              <Form>
+                <Form.Group controlId="exampleForm.ControlInput1">
+                  <Form.Control
+                    type="text"
+                    onChange={event => handleSearchText(event)}
+                    placeholder="Enter your search" />
+                </Form.Group>
+              </Form>
+            </label>
+          </div>
+        </div>
         <div className="col-lg-2 col-md-3 text-end">
           <Button
             variant="primary"
