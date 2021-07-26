cc7b791fe7864cb3d7fa982adc950c7468ff5c18
Natalie <nataly.senyk@gmail.com>
2021-07-26 12:57:04 +0300

    merged Bohdans changes

--- src/views/order-confirmation/index.jsx
+++ src/views/order-confirmation/index.jsx
@@ -23,13 +23,17 @@ const OrderConfirm = () => {
   const { bindToken } = useContext(Context)
   const discountId = history.location.pathname.split("/").pop()
 
+  useEffect(() => {
+    bindToken()
+  }, [])
+
   const addresssMapper = (el) => {
     return `${el.address} ${el.city.name} ${el.city.country.name}`
   }
   const discountAddresses =
     discountLocations && discountLocations.addresses.map(addresssMapper)
   const discountCompanyAddresses =
-    discountLocations && discountLocations.company.addresses.map(addresssMapper)
+    discountLocations && discountLocations.addresses.map(addresssMapper)
   const fullAddressLocations =
     discountLocations && discountAddresses.length
       ? discountAddresses
@@ -38,9 +42,9 @@ const OrderConfirm = () => {
   const fetchData = async (url) => {
     try {
       await axiosInstance.get(url).then((response) => {
-        setExpirationDate(response.data.promoCodePeriodEnd)
-        setDiscountName(response.data.discount.name)
-        setDiscountLocations(response.data.discount)
+        setExpirationDate(response.data.periodEnd)
+        setDiscountName(response.data.name)
+        setDiscountLocations(response.data)
       })
     } catch (e) {
       setPromocodeFetchError(e.message)
@@ -48,7 +52,7 @@ const OrderConfirm = () => {
   }
 
   useEffect(() => {
-    fetchData(`/api/orders/${discountId}`)
+    fetchData(`/api/discounts/${discountId}`)
   }, [])
 
   const fetchQrCode = async (url) => {
@@ -70,9 +74,6 @@ const OrderConfirm = () => {
     fetchQrCode(`/api/orders/create/${discountId}`)
   }, [])
 
-  useEffect(() => {
-    bindToken()
-  }, [])
 
   return (
     <div className="order-wrapper">
