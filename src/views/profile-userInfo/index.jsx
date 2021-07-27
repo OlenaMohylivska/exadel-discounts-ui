81102371f55c7ae5777e4157175d66d400fb8f99
Natalie <nataly.senyk@gmail.com>
2021-07-27 17:12:45 +0300

    waiting for Subscription GetAll endpoint, everything else is ready

--- src/views/profile-userInfo/index.jsx
+++ src/views/profile-userInfo/index.jsx
@@ -10,9 +10,10 @@ import userProfileImg from "../../assets/userProfileImg.png"
 
 function ProfileUserInfo() {
   const [categories, setCategories] = useState([])
-  const [category, setCategory] = useState([])
+  const [selectedCategories, setSelectedCategories] = useState([])
   const [successMessage, setSuccessMessage] = useState(false)
   const [fetchError, setFetchError] = useState(null)
+  const [categoryToBeDeleted, setCategoryToBeDeleted] = useState(null)
 
   const { bindToken } = useContext(Context)
 
@@ -27,6 +28,8 @@ function ProfileUserInfo() {
       .catch((err) => setFetchError(err.message))
   }, [])
 
+  useEffect(() => {})
+
   const categoriesOptions =
     categories.length > 0 &&
     categories.map((category) => ({
@@ -36,12 +39,19 @@ function ProfileUserInfo() {
     }))
 
   const handleChangeCategory = (e) => {
-    setCategory(e)
+    setSelectedCategories(e)
   }
 
   const subscriptionClickHandler = () => {
-    axiosInstance.put(`/api/employee/${category.id}/subscriptions/add`, {...[category.id]})
+    axiosInstance.put(
+      `/api/employee/subscriptions/add`,
+      selectedCategories.map((category) => category.id)
+    )
     setSuccessMessage(true)
+  }
+
+  const deleteSubscriptionHandler = (e) => {
+    setCategoryToBeDeleted(e)
   }
 
   return (
@@ -51,26 +61,29 @@ function ProfileUserInfo() {
         <div className="profile">
           <Col lg={5} className="profile-info">
             <h4 className="personal-info-title">Personal Info</h4>
-            <img
-              className="profile-img"
-              src={userProfileImg}
-              alt="user-image"
-            />
-
-            <div>
-              First name: <span className="filled-in">John</span>
-            </div>
-            <div>
-              Last name: <span className="filled-in">Brown </span>
-            </div>
-            <div className="location">
-              Location: <span className="filled-in">Lviv, </span>
-              <span className="filled-in">Ukraine</span>
+            <div className="personal-info">
+              <img
+                className="profile-img"
+                src={userProfileImg}
+                alt="user-image"
+              />
+              <div className="user-details">
+                <div>
+                  First name: <span className="filled-in">John</span>
+                </div>
+                <div>
+                  Last name: <span className="filled-in">Brown </span>
+                </div>
+                <div className="location">
+                  Location: <span className="filled-in">Lviv, </span>
+                  <span className="filled-in">Ukraine</span>
+                </div>
+              </div>
             </div>
           </Col>
           <Col lg={4} className="user-subscriptions">
             <h4 className="subscriptions-title">Manage my subscriptions</h4>
-            <h5 className="subscriptions-title">Choose by category</h5>
+            <h5 className="category-title">Choose by category</h5>
             {categoriesOptions && (
               <>
                 <Select
@@ -78,7 +91,7 @@ function ProfileUserInfo() {
                   theme="primary75"
                   options={categoriesOptions}
                   onChange={(e) => handleChangeCategory(e)}
-                  value={category}
+                  value={selectedCategories}
                   placeholder="Select..."
                   isMulti
                 />
@@ -90,6 +103,25 @@ function ProfileUserInfo() {
                 </Button>
               </>
             )}
+            <h5 className="category-title">
+              Delete previous subscriptions (if any)
+            </h5>
+            <Select
+              className="subscription-category"
+              theme="primary75"
+              options={selectedCategories}
+              onChange={(e) => handleChangeCategory(e)}
+              value={categoryToBeDeleted}
+              placeholder="Select..."
+              isMulti
+            />
+            <Button
+              className="subscriptions-btn"
+              variant="danger"
+              onClick={deleteSubscriptionHandler}
+            >
+              Delete subscriptions
+            </Button>
             {successMessage && (
               <ToastElement
                 className="toast-subs-position"
