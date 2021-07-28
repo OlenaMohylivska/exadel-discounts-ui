b7721edaa4e6426d9a2e3c21ca05b0a5d0610057
bogdan.taradada <reyyes98@gmail.com>
2021-07-28 13:32:20 +0300

    fix proble with favourite page

--- src/views/favourite-page/index.jsx
+++ src/views/favourite-page/index.jsx
@@ -6,16 +6,14 @@ import { Context } from "store/context"
 import FetchError from "components/fetch-error"
 import { Spinner } from "react-bootstrap"
 import ProductCard from "components/product-card"
-import Pagination from "components/pagination"
 
 // const baseUrl = process.env.REACT_APP_BASE_BACKEND_URL
 
 const FavouritePage = () => {
-  const [discounts, setDiscounts] = useState([])
+  const [discounts, setDiscounts] = useState(null)
   const [fetchError, setFetchError] = useState(null)
   const [loading, setLoading] = useState(false)
   const { bindToken } = useContext(Context)
-  const [itemsPerFavoritePage, setItemsPerFavoritePage] = useState(9)
   const [isFavorite, setIsFavorite] = useState([1])
 
   useEffect(() => {
@@ -41,7 +39,7 @@ const FavouritePage = () => {
       )}
 
       {fetchError && <FetchError error={fetchError} />}
-      {discounts && discounts.length > 0 && !loading && (
+      {discounts && discounts.length > 0 && (
         <div className="container">
           <div className="discounts-wrapper">
             {discounts.map((el) => {
@@ -55,17 +53,10 @@ const FavouritePage = () => {
               )
             })}
           </div>
-          {!loading && discounts && discounts.length > 0 (
-            <Pagination
-              favorites={discounts}
-              itemsPerFavoritePage={itemsPerFavoritePage}
-              setItemsPerFavoritePage={setItemsPerFavoritePage}
-              isFavorite={true}
-            />
-          )}
         </div>
       )}
-      {discounts.length === 0 && (
+
+      {discounts && discounts.length === 0 && (
         <div className="container d-flex flex-column align-items-center mt-2">
           <div>There is no favorite discounts.</div>
           <div>
