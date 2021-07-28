2bd3c2ea742bf63cc7002f11c5d96f68fe51d8d7
Slava Zhuravel <slavonzhuravel@gmail.com>
2021-07-28 15:09:43 +0300

    inputs and button disabled

--- src/components/create-location/index.jsx
+++ src/components/create-location/index.jsx
@@ -3,17 +3,22 @@ import { FormControl, Button } from "react-bootstrap"
 import axiosInstance from "components/api"
 import PropTypes from "prop-types"
 import "./styles.scss"
+
 const CreateLocation = ({ setLocationArr, locationArr }) => {
   const [country, setCountry] = useState(null)
   const [newLocation, setNewLocation] = useState({ address: null, city: {} })
   const [newCity, setNewCity] = useState({})
+  const [isEntered, setIsEntered] = useState(true)
   useEffect(() => {
     setNewLocation({ city: newCity })
   }, [newCity])
   const submit = () => {
     axiosInstance
       .post("/api/location", newLocation)
-      .then((res) => setLocationArr([...locationArr, res.data]))
+      .then((res) => {
+        setLocationArr([...locationArr, res.data])
+        setIsEntered(false)
+      })
   }
 
   return (
@@ -24,12 +29,14 @@ const CreateLocation = ({ setLocationArr, locationArr }) => {
           onChange={(e) => setCountry(e.target.value)}
           id="country"
           placeholder="country"
+          disabled={!isEntered}
         />
         <FormControl
           placeholder="city"
           className="form-field margin-bottom-5px"
           id="city"
           name="city"
+          disabled={!isEntered}
           onChange={(e) =>
             setNewCity({
               name: e.target.value,
@@ -45,6 +52,7 @@ const CreateLocation = ({ setLocationArr, locationArr }) => {
           id="addresses"
           name="addresses"
           value={newLocation.address ? newLocation.address : ""}
+          disabled={!isEntered}
           onChange={(e) =>
             setNewLocation({ ...newLocation, address: e.target.value })
           }
@@ -53,6 +61,7 @@ const CreateLocation = ({ setLocationArr, locationArr }) => {
           className="margin-bottom-5px"
           variant="primary"
           onClick={() => submit()}
+          disabled={!isEntered}
         >
           Apply
         </Button>
